58c29cd4e7e8f565e6ae31593820f7c04c87a178
daank <32707088+daankennes@users.noreply.github.com>
2017-11-27 15:32:42 +0100

    CameraPlugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,6 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      //qsdf
       statusBar.styleDefault();
       splashScreen.hide();
     });

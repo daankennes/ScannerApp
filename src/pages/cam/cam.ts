219ada3bf5b4183077714708466ea8b45a2d374a
daank <32707088+daankennes@users.noreply.github.com>
2017-11-27 17:53:54 +0100

    Les gedaan

--- src/pages/cam/cam.ts
+++ src/pages/cam/cam.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-cam',
@@ -8,13 +9,22 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 })
 export class CamPage {
 
-  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner) {}
+  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner, public alertCtrl: AlertController) {}
 
+  public presentAlert(barcodeData) {
+  let alert = this.alertCtrl.create({
+    title: 'Persoon toegevoegd!',
+    subTitle: 'x wordt toegevoegd aan de lijst.',
+    buttons: ['Sluiten']
+  });
+  alert.present();
+}
   public scan(){
     this.barcodeScanner.scan().then((barcodeData) => {
        console.log(barcodeData);
+       presentAlert(barcodeData);
       }, (err) => {
-          console.log(err);
+          console.log(err)
     });
   }
 }

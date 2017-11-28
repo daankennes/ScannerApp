a9781fc8edc3c8bc8307e6ff610bb2d14beb1618
daank <32707088+daankennes@users.noreply.github.com>
2017-11-28 10:49:49 +0100

    Alert werkt + storage kan gebruikt worden

--- src/pages/cam/cam.ts
+++ src/pages/cam/cam.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-cam',
@@ -9,22 +10,29 @@ import { AlertController } from 'ionic-angular';
 })
 export class CamPage {
 
-  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner, public alertCtrl: AlertController) {}
+  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner, private alertCtrl: AlertController, private storage: Storage) {
 
-  public presentAlert(barcodeData) {
-  let alert = this.alertCtrl.create({
-    title: 'Persoon toegevoegd!',
-    subTitle: 'x wordt toegevoegd aan de lijst.',
-    buttons: ['Sluiten']
-  });
-  alert.present();
-}
+  }
+
+  presentAddedPerson(barcodeData) : void {
+    let alert = this.alertCtrl.create({
+      title: 'Persoon toegevoegd!',
+      subTitle: barcodeData.text + ' wordt toegevoegd aan de lijst.',
+      buttons: ['Sluiten']
+    });
+    alert.present();
+  }
+
+
   public scan(){
     this.barcodeScanner.scan().then((barcodeData) => {
        console.log(barcodeData);
-       presentAlert(barcodeData);
+       this.storage.set('snumber', barcodeData.text); //voor nu laatst gescande value storen
+       this.presentAddedPerson(barcodeData);
       }, (err) => {
           console.log(err)
     });
   }
+
+
 }
